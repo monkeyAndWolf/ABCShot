0c4ed8af181be802a8666111a32b5f94bfe2aaea
Rebecca Worledge <rebecca.worledge@qt.io>
2016-09-24 20:43:42 -0700

    Rescaling for the default tune setting. It's now bigger as standard.

--- abcshots.c
+++ abcshots.c
@@ -14,15 +14,23 @@ int runAbcToSvg(char *abcFile, char *svgFile)
 
     char *first = "abcm2ps";
     char *second = "-g";
-    char *third = "-O";
-    char **argc= malloc(sizeof(char*)*5);
+    char *third = "-s";
+    char *fourth = "2";
+    char *fifth = "-w";
+    char *sixth =  "15.28in";
+    char *seventh = "-O";
+    char **argc= malloc(sizeof(char*)*9);
     argc[0] = first;
     argc[1] = second;
     argc[2] = third;
-    argc[3] = svgFile;
-    argc[4] = abcFile;
+    argc[3] = fourth;
+    argc[4] = fifth;
+    argc[5] = sixth;
+    argc[6] = seventh;
+    argc[7] = svgFile;
+    argc[8] = abcFile;
 
-    int answer = createAbc(5, argc);
+    int answer = createAbc(9, argc);
     free(argc);
     return answer;
 }
